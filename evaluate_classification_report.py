8907c5897283539d13ab78f2a8a9c96d3268e996
Matej Galic <matej.galic.ets@gmail.com>
2025-06-19 18:13:31 +0200

    added counting trainable parameters of models and loss curves and confusion matrixes of models

--- evaluate_classification_report.py
+++ evaluate_classification_report.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import torch
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 from data_loader import create_dataloaders
-from model_utils import build_model, validate_model
+from model_utils import build_model, validate_model, build_efficientnet, build_densenet
 
 CSV_PATH = 'plates_dataset/plates.csv'
 MODEL_PATH = 'best_model.pth'
@@ -16,11 +16,15 @@ model = build_model(num_classes=len(label_map))
 model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
 model = model.to(DEVICE)
 
+# Count the number of trainable parameters
+total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Total trainable parameters in the model: {total_params}")
+
 val_acc, val_labels, val_preds, _ = validate_model(model, valid_loader, DEVICE, criterion=None)
 print(f"Validation accuracy: {val_acc:.4f}")
 
 report = classification_report(val_labels, val_preds, target_names=list(label_map.values()))
-with open("classification_report.txt", "w") as f:
+with open("classification_report_resnet50.txt", "w") as f:
     f.write(report)
 
 cm = confusion_matrix(val_labels, val_preds)
